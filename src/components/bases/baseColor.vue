404c256d77ffcea3e4606fa782958b99d34a772a
Meawmfia <boon20002543@hotmail.com>
2021-04-23 23:23:52 +0700

    [UPDATE] demo choose color

---
+++ src/components/bases/baseColor.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div class="grid grid-cols-3 grid-rows-5 gap-10 ml-48 mt-28">
+    <div v-for="color of colors" :key="color">
+      <button :style="{backgroundColor: color}" class="w-32 h-20 text-white">{{ color }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      colors: {
+        red: "#E74C3C",
+        green: "#27AE60",
+        blue: "#2E86C1",
+        purple: "#8E44AD",
+        yello: "#F4D03F",
+        grey: "#707B7C",
+        black: "#000",
+        white: "#FFFF",
+        brown: "#762400",
+      },
+    };
+  },
+};
+</script>
--- src/views/Color.vue
+++ src/views/Color.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <Color />
 </template>
+<script>
+  import Color from '@/components/bases/baseColor.vue'
+
+export default {
+
+  components:{
+    Color
+  }
+}
+
+</script>
