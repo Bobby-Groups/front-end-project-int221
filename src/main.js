e1f39466aba1508eacf26ef57cf4b36e3687b61f
Meawmfia <boon20002543@hotmail.com>
2021-04-24 21:49:45 +0700

    [UPDATE] global VueAxios

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/tailwind.css'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(VueAxios,axios).mount('#app')
