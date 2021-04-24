49487aa87763fd4cda06efcbf682b3b30f6fd43a
Meawmfia <boon20002543@hotmail.com>
2021-04-24 23:00:21 +0700

    [UPDATE] baseColor getData, global mixins

--- src/components/bases/baseColor.vue
+++ src/components/bases/baseColor.vue
@@ -1,28 +1,29 @@
 <template>
 
   <div class="grid grid-cols-3 grid-rows-5 gap-10 ml-48 mt-28">
-    <div v-for="color of colors" :key="color">
-      <button :style="{backgroundColor: color}" class="w-32 h-20 text-white">{{ color }}</button>
+    <div v-for="color of colors" :key="color.id">
+      <button :style="{backgroundColor: color.colorvalue}" class="w-32 h-20 text-white">{{ color }}</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+
   data() {
     return {
-      colors: {
-        red: "#E74C3C",
-        green: "#27AE60",
-        blue: "#2E86C1",
-        purple: "#8E44AD",
-        yello: "#F4D03F",
-        grey: "#707B7C",
-        black: "#000",
-        white: "#FFFF",
-        brown: "#762400",
-      },
+      colors : []
     };
   },
+  async created(){
+    this.colors = await this.fetchColor() 
+    console.log(this.colors);
+  },
+  methods:{
+    async fetchColor(){
+       const { data } = await this.getData('color')
+       return data
+    }
+  }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -5,5 +5,6 @@ import store from './store'
 import './assets/tailwind.css'
 import VueAxios from 'vue-axios'
 import axios from 'axios'
+import api from './mixins/api'
 
-createApp(App).use(store).use(router).use(VueAxios,axios).mount('#app')
+createApp(App).use(store).use(router).use(VueAxios,axios).mixin(api).mount('#app')
