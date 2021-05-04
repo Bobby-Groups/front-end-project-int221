ef9203493e854dcc034bb636647417f497c78d27
Meawmfia <boon20002543@hotmail.com>
2021-05-04 22:16:52 +0700

    [UPDATE] brand get data

--- src/components/bases/baseBrand.vue
+++ src/components/bases/baseBrand.vue
@@ -1,16 +1,48 @@
 <template>
-  <div class="flex flex-col">
-    <div v-for="brand of brands" :key="brand.id">
-      <div class="flex flex-row border-4">
-        <div class="border-4 border-black p-4">
-          {{ brand.brandName }}
-        <p v-for="type of brand.types" :key="type.id">
-          {{ type.typename }}
-        </p>
+  <div class="min-h-screen overflow-hidden">
+    <div class="flex flex-col ml-32">
+      <div v-for="brand of brands" :key="brand.id">
+        <div class="border-4">
+          <b>{{ brand.brandName }}</b>
+          <div class="flex flex-row">
+            <div
+              v-for="type of brand.types"
+              :key="type.id"
+              class="border-2 border-black p-4"
+            >
+              <table class="text-left text-xs">
+                <tr>
+                  <span> {{ type.typename }} </span>
+                </tr>
+                <tr>
+                  <img
+                    src="@/assets/TeamPhoto/bird.jpg"
+                    alt="bird"
+                    class="w-32 h-32"
+                  />
+                </tr>
+                <tr>
+                  <td>
+                    <div>Price: {{ type.price }}</div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <div class="w-40">description: {{ type.description }}</div>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </div>
         </div>
-
       </div>
+     
     </div>
+     <router-link
+        to="/total"
+        class="p-5 border-4 bg-black text-white rounded-xl"
+        >CONFIRM</router-link
+      >
   </div>
 </template>
 
